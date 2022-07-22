c7cdf0f2d3baa015859004d37fc92dc6c7fe5147
Matthew T. Renwick <mrenny65@gmail.com>
2022-07-22 11:04:59 -0600

    added form to sales.html, updated/fixed index.html

--- js/app.js
+++ js/app.js
@@ -70,9 +70,40 @@ function cityTable(){
 cityTable();
 
 
-City.prototype.buildSalesRow = function(){
+// City.prototype.buildSalesRow = function(){
 
-
+function generate_table() {
+  // creates a <table> element and a <tbody> element
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
+   
+    // creating all cells
+    for (let i = 0; i < 2; i++) {
+    // creates a table row
+    const row = document.createElement("tr");
+   
+    for (let j = 0; j < 2; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+    const cell = document.createElement("td");
+    const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+   
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+   
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  document.body.appendChild(tbl);
+  // sets the border attribute of tbl to '2'
+  tbl.setAttribute("border", "2");
+}
+generate_table();  
 
 
 // this adds a row to the table
@@ -83,7 +114,7 @@ City.prototype.buildSalesRow = function(){
 // appdend the td to the tr for the city
 // after the loop is finished, append the tr to the table
 
-}
+
 
   // let divElem = document.createElement('store');
   // storeDiv.appendChild(divElem);
@@ -111,7 +142,7 @@ City.prototype.buildSalesRow = function(){
   // let th1Elem = document.createElement('th');
   // th1Elem.textContent = ;
 
-// }
+// 
 
 
 
